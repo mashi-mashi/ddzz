8f3d5979df0b3e3a5be8e583dee335d9a6db263f
mashi-mashi <shima.trends@gmail.com>
2021-05-25 01:06:45 +0900

    update auth

---
+++ lib/src/infrastructure/firebase_reference.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ddzz/src/features/article_provider.dart';
+import 'package:ddzz/src/features/auth_controller.dart';
+
+class FirestoreReference {
+  static String get _userId => AuthController().user?.uid ?? '';
+  static String base() => 'deluca/v1/';
+  static CollectionReference providers() => FirebaseFirestore.instance
+      .collection(FirestoreReference.base() + 'providers');
+
+  static CollectionReference articles() {
+    var collectionReference = FirebaseFirestore.instance
+        .collection(FirestoreReference.base() + 'articles')
+        .withConverter<Article>(
+          fromFirestore: (snapshot, _) =>
+              Article.fromJson(snapshot.id, snapshot.data()!), toFirestore: (model, _) => Article.toJson(model)),
+        );
+    return collectionReference;
+  }
+
+  static CollectionReference providerArticles(String providerId) =>
+      FirebaseFirestore.instance.collection(
+          FirestoreReference.base() + 'providers/$providerId/articles');
+
+  static CollectionReference userSubscriptions() {
+    return FirebaseFirestore.instance.collection(FirestoreReference.base() +
+        'users/' +
+        FirestoreReference._userId +
+        '/subscriptions');
+  }
+
+  static CollectionReference userPicks() {
+    return FirebaseFirestore.instance.collection(FirestoreReference.base() +
+        'users/' +
+        FirestoreReference._userId +
+        '/picks');
+  }
+}
---
+++ lib/src/infrastructure/firestore.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Firestore {
+  static FieldValue timestamp() {
+    return FieldValue.serverTimestamp();
+  }
+
+  static Future<T> get<T>(DocumentReference ref) async {
+    final doc = await ref.get();
+    return doc.data() as T;
+    // return {...?doc.data(), 'id': doc.id} as T;
+  }
+
+  static Future<Iterable<T>> getByIds<T>(
+      CollectionReference ref, List<String> ids) async {
+    final uniq = ids.toSet().toList();
+
+    if (uniq.isEmpty) return [];
+
+    return Future.wait(uniq.map((id) => Firestore.get<T>(ref.doc(id))));
+  }
+
+  static Future<List<T>> getByQuery<T>(Query query) async {
+    final snap = await query.get();
+    final data = snap.docs.map((doc) => doc.data() as T);
+    print('firestore query size - ${data.length.toString()}');
+    return data.toList();
+  }
+
+  static Stream<QuerySnapshot> getSnapshotByQuery(Query query) {
+    return query.snapshots();
+  }
+
+  static Future<void> delete(DocumentReference ref) async {
+    return await Firestore.set(
+        ref, {'deleted': true, 'deletedAt': Firestore.timestamp()});
+  }
+
+  static Future<void> forceDelete(DocumentReference ref) async {
+    return await ref.delete();
+  }
+
+  static Future<void> add(
+      DocumentReference ref, Map<String, dynamic> data) async {
+    return await ref.set({
+      ...data,
+      'deleted': false,
+      'createdAt': Firestore.timestamp(),
+      'updatedAt': Firestore.timestamp()
+    }, SetOptions(merge: true));
+  }
+
+  static Future<void> set(
+      DocumentReference ref, Map<String, dynamic> data) async {
+    return await ref.set(
+        {...data, 'updatedAt': Firestore.timestamp()}, SetOptions(merge: true));
+  }
+}
